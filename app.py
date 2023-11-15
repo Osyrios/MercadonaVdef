192427a344125453c3619c2109be25b1635a4ce4
Yassine <yassine.n13@gmail.com>
2023-11-15 12:04:14 +0100

    Commit et push du projet au 15/11 12:04

--- app.py
+++ app.py
@@ -18,42 +18,32 @@ from config.config import Config
 from database.database import db
 from datetime import timedelta
 
+app = Flask(__name__)
+app.config.from_object(Config)  # import la configuration de l'app
+app.permanent_session_lifetime = timedelta(hours=2)
+# Import des routes Blueprint
+app.register_blueprint(product_ws)
+app.register_blueprint(category_ws)
+app.register_blueprint(homepage_bp)
+app.register_blueprint(catalogue_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(back_office_bp)
+app.register_blueprint(product_update_bp)
+app.register_blueprint(new_category_bp)
+app.register_blueprint(logout_bp)
+app.register_blueprint(admin_ws)
+app.register_blueprint(new_admin_bp)
+db.init_app(app)
+# verifie la presence des table de l'application et les créer si ce n'est pas le cas
+with app.app_context():
+    if not inspect(db.engine).has_table('category'):
+        Category.__table__.create(bind=db.engine)
 
-def create_app():
-    app = Flask(__name__)
+    if not inspect(db.engine).has_table('products'):
+        Products.__table__.create(bind=db.engine)
 
-    app.config.from_object(Config)  # import la configuration de l'app
-
-    app.permanent_session_lifetime = timedelta(hours=2)
-
-    # Import des routes Blueprint
-    app.register_blueprint(product_ws)
-    app.register_blueprint(category_ws)
-    app.register_blueprint(homepage_bp)
-    app.register_blueprint(catalogue_bp)
-    app.register_blueprint(login_bp)
-    app.register_blueprint(back_office_bp)
-    app.register_blueprint(product_update_bp)
-    app.register_blueprint(new_category_bp)
-    app.register_blueprint(logout_bp)
-    app.register_blueprint(admin_ws)
-    app.register_blueprint(new_admin_bp)
-
-    db.init_app(app)
-
-    # verifie la presence des table de l'application et les créer si ce n'est pas le cas
-    with app.app_context():
-        if not inspect(db.engine).has_table('category'):
-            Category.__table__.create(bind=db.engine)
-
-        if not inspect(db.engine).has_table('products'):
-            Products.__table__.create(bind=db.engine)
-
-        if not inspect(db.engine).has_table('admins'):
-            Admins.__table__.create(bind=db.engine)
-
-    return app
-
+    if not inspect(db.engine).has_table('admins'):
+        Admins.__table__.create(bind=db.engine)
 
 if __name__ == '__main__':
-    create_app().run()
+    app.run()
