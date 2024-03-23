860d1e845e7d3cee0235dd1a6822d871a2d5d68e
Yassine <yassine.n13@gmail.com>
2024-03-23 15:26:01 +0100

    redesign : base template for header and footer done, index template started

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        'RobotoBold':['robotoBold']
+        'RobotoBold':['robotoBold'],
+        'RobotoItalic':['robotoItalic'],
+        'RobotoRegular':['robotoRegular']
       }
     },
   },
